419750cbf44fbd5acd70f380e42bfd419d148965
soniasaber <sonia.saber2016@gmail.com>
2018-06-17 01:46:19 -0400

    print test and timestamp

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -67,7 +68,7 @@ public class DiceJobSearch {
 
 		driver.close();
 		
-		
+		System.out.println("Test Completed -" + LocalDateTime.now());
 		
 		
 		
